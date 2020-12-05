c5fe4bfc91180db1be87240b80aa5c0db72aead7
Dan Carpenter <drdcarpenter@googlemail.com>
2020-12-05 20:23:28 +0000

    sort reactive code in app

--- app.R
+++ app.R
@@ -11,6 +11,8 @@ library(shiny)
 library(shinydashboard)
 library(leaflet)
 library(readr)
+library(dplyr)
+library(DT)
 
 street_trees <- read_csv("street_trees.csv")
 
@@ -19,10 +21,9 @@ ui <- fui <- dashboardPage(
     dashboardHeader(title = "Street trees"),
     
     dashboardSidebar(
-        radioButtons("centre", "Distance between trees (m)",
-                     choices = list("7" = 1,
-                                    "14" = 2,
-                                    "28" = 3))
+        selectInput("centre", "Distance between trees (m)",
+                     choices = list(7, 14, 28),
+                    multiple = FALSE)
     ),
     
     dashboardBody(
@@ -36,22 +37,38 @@ ui <- fui <- dashboardPage(
         fluidRow(
             
             leafletOutput("map")
+        ),
+        
+        fluidRow(
+            
+            DTOutput("table")
         )
     )
 )
 
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    #output$treesBox <- renderValueBox()
+    st <- eventReactive(input$centre, {
+        dplyr::filter(street_trees, centres == input$centre)
+    })
+    
+    # output$treesBox <- renderValueBox(
+    #     valueBox(
+    #     "Number of trees",
+    #     count(st()),
+    #     icon = icon("tree"))
+    # )
     
     #output$parkingspacesBox <- renderValueBox()
     
     output$map <- renderLeaflet({
-        leaflet() %>% 
-            addTiles() %>% 
-            setView(-0.94680369, 51.456922, zoom = 16) %>% 
-            addCircleMarkers(lat = ~y, lng = ~x, data = street_trees, radius = 1, color = "green")
+        leaflet() %>%
+            addTiles() %>%
+            setView(-0.94680369, 51.456922, zoom = 16) %>%
+            addCircleMarkers(lat = ~y, lng = ~x, data = st(), radius = 1, color = "green")
     })
+    
+    output$table <- renderDT({st()})
 }
 
 shinyApp(ui, server)
